9e1881329c0c4e0edaeaa166a009dfe1d19e6464
Mahir Shahriyar <71180348+Mahir-Shahriyar@users.noreply.github.com>
2021-03-18 21:57:16 +0600

    update cox.php and add more feature

--- cox.php
+++ cox.php
@@ -9,7 +9,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-        <title>Event</title>
+        <title>Booking</title>
 
         <!-- Google Font -->
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
@@ -74,7 +74,18 @@
                                     <li class="nav-item">
                                         <a class="nav-link" href="index.html">Home</a>
                                     </li>
-
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="about.html">About us</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="menu.html">Menu</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="gallery.html">Gallery</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link active" href="events.html">Booking</a>
+                                    </li>
                                     <li class="nav-item">
                                         <a class="nav-link" href="contact.html">Contact</a>
                                     </li>
@@ -91,19 +102,91 @@
 
         <!-- Banner Section Ends Here -->
 
-        <!-- booking section start -->
+        <!-- Birthday Event Section Start Here -->
+        <section class="birthday-event">
+            <div class="container">
+                <div class="row">
 
-        <!-- booking section start Ends Here -->
-        <h2>Radio Buttons</h2>
+                    <div class="col-md-4 col-sm-4">
+                        <a href="cox.php">
+                        <div class="card-deck">
+                            <div class="card">
+                                <img class="card-img-top" src="images/hotel.jpg">
+                                <div class="card-body">
+                                    <h3>Hotel</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="card-deck">
+                            <div class="card">
+                                <img class="card-img-top" src="images/place.jpg">
+                                <div class="card-body">
+                                    <h3>Other Places</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="card-deck">
+                            <div class="card">
+                                <img class="card-img-top" src="images/coxtransport.jpg">
+                                <div class="card-body">
+                                    <h3>Transport</h3>
 
-        <form>
-          <input type="radio" id="male" name="gender" value="male">
-          <label for="male">Male</label><br>
-          <input type="radio" id="female" name="gender" value="female">
-          <label for="female">Female</label><br>
-          <input type="radio" id="other" name="gender" value="other">
-          <label for="other">Other</label>
-        </form>   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="birthday-event">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4 col-sm-4">
+                        <div class="card-deck">
+                            <div class="card">
+                                <img class="card-img-top" src="images/birthday-events1.jpg">
+                                <div class="card-body">
+                                    <h3>Birthday Party</h3>
+                                    <h5>8 pm</h5>
+                                    <p>Date:02 August 2020</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="card-deck">
+                            <div class="card">
+                                <img class="card-img-top" src="images/birthday-events2.jpg">
+                                <div class="card-body">
+                                    <h3>Birthday Party</h3>
+                                    <h5>8 pm</h5>
+                                    <p>Date:03 August 2020</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                        <div class="card-deck">
+                            <div class="card">
+                                <img class="card-img-top" src="images/birthday-events3.jpg">
+                                <div class="card-body">
+                                    <h3>Birthday Party</h3>
+                                    <h5>8 pm</h5>
+                                    <p>Date:04 August 2020</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Birthday Event Section Ends Here -->
 
         <!-- MARRIAGE Event Section Start Here -->
 
