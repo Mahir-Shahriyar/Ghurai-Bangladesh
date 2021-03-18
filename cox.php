66b83e8e9d00f240ac852f2585dd04f7791c411c
Mahir Shahriyar <71180348+mahirshahriyar066@users.noreply.github.com>
2021-03-18 11:32:03 +0600

    cox.php information forms page added initial

---
+++ cox.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <title>Event</title>
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+        <!-- Owl Carousel Link -->
+        <link rel="stylesheet" href="https://cdn.boomcdn.com/libs/owl-carousel/2.3.4/assets/owl.theme.default.min.css">
+        <link rel="stylesheet" href="https://cdn.boomcdn.com/libs/owl-carousel/2.3.4/assets/owl.carousel.min.css">
+
+        <!-- Custom css -->
+        <link rel="stylesheet" href="css/custom.css">
+
+        <!-- Responsive Style -->
+	    <link rel="stylesheet" href="css/responsive.css">
+    </head>
+
+    <body>
+        <!-- Header Top Section Start Here -->
+        <section class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="call-info">
+                            <p>Call for Reservation :
+                                <a href="tel:017711-208-4041">01771-120840-41 </a> /
+                                <a href="tel:024-895-5952">+88 02 48955952</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="social-icon text-right">
+                            <ul>
+                                <li><a href="https://www.google.com/"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://www.google.com/"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="https://www.google.com/"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="https://www.google.com/"><i class="fab fa-google"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Header Top Section Ends Here -->
+
+        <!-- Header Section Start Here -->
+        <header class="header" id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-expand-lg">
+                            <a class="navbar-brand" href="#"><img src="images/Jenenin-site-logo-2.png" alt="Logo"></a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <!-- <span class="navbar-toggler-icon"></span> -->
+                                <span class="sr-only">Toggle navigation </span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.html">Home</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contact.html">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                          </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Header Section Ends Here -->
+
+        <!-- Events Section Start Here -->
+
+        <!-- Banner Section Ends Here -->
+
+        <!-- booking section start -->
+
+        <!-- booking section start Ends Here -->
+        <h2>Radio Buttons</h2>
+
+        <form>
+          <input type="radio" id="male" name="gender" value="male">
+          <label for="male">Male</label><br>
+          <input type="radio" id="female" name="gender" value="female">
+          <label for="female">Female</label><br>
+          <input type="radio" id="other" name="gender" value="other">
+          <label for="other">Other</label>
+        </form>   
+
+        <!-- MARRIAGE Event Section Start Here -->
+
+        <!-- MARRIAGE Event Section Ends Here -->
+
+        <!-- Footer Section Start Here -->
+
+        <!-- Footer Section Ends Here -->
+
+        <!-- Optional JavaScript -->
+
+        <!-- jQuery first, then , then  -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <!-- Popper.js -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <!-- Bootstrap JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/8d5b29c7bc.js" crossorigin="anonymous"></script>
+
+        <script src="js/isotope.pkgd.min.js"></script>
+        <!-- Custom Js -->
+        <script src="js/custom.js"></script>
+    </body>
+
+</html>
