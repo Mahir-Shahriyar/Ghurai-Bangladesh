6cd2d39adc0e3af5df0daa4eecfcd05f0942b3f6
NuzhatZahan <76831249+NuzhatZahan@users.noreply.github.com>
2021-03-20 23:56:42 +0600

    Dummy Values are added in database file
    The previous sql file have been updated using some dummy values.

--- database/ghuraibangladesh.sql
+++ database/ghuraibangladesh.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE ghuraibangladesh
+
 CREATE TABLE Hotel (Hotel_id int(10) NOT NULL AUTO_INCREMENT, hotel_type varchar(30) NOT NULL, hotel_location varchar(30) NOT NULL, hotel_rent int(10) NOT NULL, HotelHotel_id int(10) NOT NULL, Regionregion_id int(10) NOT NULL, PRIMARY KEY (Hotel_id));
 CREATE TABLE Booking (booking_id int(10) NOT NULL AUTO_INCREMENT, booking_for_person int(10) NOT NULL, Personperson_id int(10) NOT NULL, HotelHotel_id int(10) NOT NULL, PRIMARY KEY (booking_id));
 CREATE TABLE Person (person_id int(10) NOT NULL AUTO_INCREMENT, person_name varchar(30) NOT NULL, person_email varchar(30) NOT NULL UNIQUE, person_phone varchar(30) NOT NULL, person_role binary(2) NOT NULL, PRIMARY KEY (person_id));
@@ -12,3 +14,8 @@ ALTER TABLE Booking ADD CONSTRAINT FKBooking956763 FOREIGN KEY (HotelHotel_id) R
 ALTER TABLE Region_Booking ADD CONSTRAINT FKRegion_Boo568837 FOREIGN KEY (Regionregion_id) REFERENCES Region (region_id);
 ALTER TABLE Region_Booking ADD CONSTRAINT FKRegion_Boo630931 FOREIGN KEY (Bookingbooking_id) REFERENCES Booking (booking_id);
 ALTER TABLE Destination ADD CONSTRAINT FKDestinatio431438 FOREIGN KEY (Regionregion_id) REFERENCES Region (region_id);
+ALTER TABLE hotel ADD COLUMN hotel_name VARCHAR(30);
+ALTER TABLE `person` CHANGE `person_role` `person_role` INT(2) NOT NULL DEFAULT '0';
+Alter table booking Drop FOREIGN KEY FKBooking205104;
+Alter table booking Drop COLUMN Personperson_id;
+ALTER TABLE `booking`  ADD `person_email` VARCHAR(30) NOT NULL  AFTER `booking_for_person`;
